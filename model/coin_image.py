583e2ee0e873dde05ed8dc5c1e3d22772006f7bc
matrackif <filip.matracki@gmail.com>
2019-02-04 17:39:47 +0100

    Remove duplicate constructor

--- model/coin_image.py
+++ model/coin_image.py
@@ -2,11 +2,6 @@ from model.enums import CoinLabel
 
 
 class CoinImage:
-    def __init__(self, radius: int):
-        self.radius = radius
-        self.label = CoinLabel.DUMMY
-
-    def __init__(self, radius: int, label: CoinLabel):
+    def __init__(self, radius: int, label: CoinLabel = CoinLabel.DUMMY):
         self.radius = radius
         self.label = label
-
