c1610b75fa18daa6b7021ea1fe824abb0c4974fc
Ivan Matyazh <ivan070297@gmail.com>
2019-02-06 22:46:11 +0200

    Add tolerance rate for the postprocessing

--- model/postprocessing.py
+++ model/postprocessing.py
@@ -2,6 +2,8 @@ from model.enums import Coin, CoinLabel
 from model.coin_image import CoinImage
 from typing import List
 
+TOLERANCE_RATE = 1
+
 
 def get_total_count(coins: List[CoinImage]) -> int:
     total = 0
@@ -19,7 +21,6 @@ def convert_label_to_coin(coin: CoinImage, coins: List[CoinImage]) -> Coin:
     return Coin[coin.label.name[:-5]]
 
 
-"""TODO add toleration for radius"""
 def analyse_coin(coin: CoinImage, coins: List[CoinImage]) -> Coin:
     similar_coins = list(filter(lambda x: is_similar_coin_known(coin, x), coins))
     if len(similar_coins) != 0:
@@ -29,9 +30,8 @@ def analyse_coin(coin: CoinImage, coins: List[CoinImage]) -> Coin:
     return get_coin(coin, similar_class_coins)
 
 
-"""TODO add toleration for radius"""
 def is_similar_coin_known(coin: CoinImage, iter_coin: CoinImage) -> bool:
-    if (coin.radius == iter_coin.radius
+    if (iter_coin.radius - TOLERANCE_RATE <= coin.radius <= iter_coin.radius + TOLERANCE_RATE
             and iter_coin.label != CoinLabel.ONE_TWO_FIVE_TAIL
             and iter_coin.label != CoinLabel.TEN_TWENTY_FIFTY_ONE_TAIL):
         return True
@@ -44,20 +44,25 @@ def is_similar_label_coin(coin: CoinImage, iter_coin: CoinImage) -> bool:
     return False
 
 
-"""TODO add toleration for radius"""
 def unique(coins: List[CoinImage]) -> List[CoinImage]:
     unique_coins = []
     for coin in coins:
-        if coin not in unique_coins:
+        if is_unique_coin(coin, unique_coins):
             unique_coins.append(coin)
     return unique_coins
 
 
-"""TODO add toleration for radius"""
+def is_unique_coin(coin:CoinImage, unique_coins: List[CoinImage]) -> bool:
+    for unique_coin in unique_coins:
+        if unique_coin.radius - TOLERANCE_RATE <= coin.radius <= unique_coin.radius + TOLERANCE_RATE:
+            return False
+    return True
+
 def get_coin(coin: CoinImage, similar_class_coins: List[CoinImage]) -> Coin:
     index = 0
     for i in range (0, len(similar_class_coins)):
-        if coin.radius == similar_class_coins[i].radius:
+        if (similar_class_coins[i].radius + TOLERANCE_RATE >= coin.radius >=
+                similar_class_coins[i].radius - TOLERANCE_RATE):
             index = i
             break
     if coin.label == CoinLabel.ONE_TWO_FIVE_TAIL:
