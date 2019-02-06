dab821fa780bb20a94f6618cc8b080f754618733
Ivan Matyazh <ivan070297@gmail.com>
2019-02-06 21:52:45 +0200

    Add OpenCV Hough transform

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import model.coin_image as ci
 
 
 def normalize(x_data: np.ndarray):
@@ -21,72 +22,41 @@ def apply_gaussian_filter(img):
     return dst
 
 
-"""TODO verification of algorithm"""
-"""
-source: https://github.com/SunilVasu/Circle-Hough-Transform/blob/master/HoughTransform_FOR_GIVEN_TEST_IMAGE.py
-def detect_circles(img):
-    circles = []
-    rows = img.shape[0]
-    cols = img.shape[1]
+def hough_transform(img, width, height):
+    ret, dst = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    circles = cv2.HoughCircles(dst, cv2.HOUGH_GRADIENT, dp=1, minDist=10, maxRadius=50, param1=50, param2=30,
+                               minRadius=10)
+    circles = filter_found_cicles((np.around(circles))[0])
+    coins = []
+    coin_images = []
+    for circle in circles:
+        x = int(circle[0] - circle[2])
+        y = int(circle[1] - circle[2])
+        local_height = int(2 * circle[2])
+        local_width = int(2 * circle[2])
+        coin_found = img[y:y + local_height, x:x + local_width]
+        coins.append(cv2.resize(coin_found, (width, height)))
+        coin_images.append(ci.CoinImage(int(circle[2])))
+    return coins, coin_images
 
-    # initializing the angles to be computed
-    sinang = dict()
-    cosang = dict()
 
-    # initializing the angles
-    for angle in range(0, 360):
-        sinang[angle] = np.sin(angle * np.pi / 180)
-        cosang[angle] = np.cos(angle * np.pi / 180)
+def filter_found_cicles(circles):
+    ordered_circles = sorted(circles, key=lambda x: x[2])
+    filtered_circles = []
+    for i in range(len(ordered_circles) - 1, 0, -1):
+        is_contained = False
+        for circle in filtered_circles:
+            if distance(circle, ordered_circles[i]) < circle[2]:
+                is_contained = True
+                break
+        if not is_contained:
+            filtered_circles.append(ordered_circles[i])
+    return filtered_circles
 
-        # initializing the different radius
-    # For Given Test Image <----------------------PLEASE SEE BEFORE RUNNING------------------------------->
-    radius = [i for i in range(10, 70)]
-    # For Generic Images
-    # length=int(rows/2)
-    # radius = [i for i in range(5,length)]
 
-    # Initial threshold value
-    threshold = 190
+def distance(circle1, circle2):
+    dif_x = (circle1[0] - circle2[0])**2
+    dif_y = (circle1[1] - circle2[1])**2
+    return (dif_x + dif_y)**(1/2.0)
 
-    for r in radius:
-        # Initializing an empty 2D array with zeroes
-        acc_cells = np.full((rows, cols), fill_value=0, dtype=np.uint64)
-
-        # Iterating through the original image
-        for x in range(rows):
-            for y in range(cols):
-                if img[x][y] == 255:  # edge
-                    # increment in the accumulator cells
-                    for angle in range(0, 360):
-                        b = y - round(r * sinang[angle])
-                        a = x - round(r * cosang[angle])
-                        if a >= 0 and a < rows and b >= 0 and b < cols:
-                            acc_cells[a][b] += 1
-
-        print('For radius: ', r)
-        acc_cell_max = np.amax(acc_cells)
-        print('max acc value: ', acc_cell_max)
-
-        if (acc_cell_max > 150):
-
-            print("Detecting the circles for radius: ", r)
-
-            # Initial threshold
-            acc_cells[acc_cells < 150] = 0
-
-            # find the circles for this radius
-            for i in range(rows):
-                for j in range(cols):
-                    if (i > 0 and j > 0 and i < rows - 1 and j < cols - 1 and acc_cells[i][j] >= 150):
-                        avg_sum = np.float32((acc_cells[i][j] + acc_cells[i - 1][j] + acc_cells[i + 1][j] +
-                                              acc_cells[i][j - 1] + acc_cells[i][j + 1] + acc_cells[i - 1][j - 1] +
-                                              acc_cells[i - 1][j + 1] + acc_cells[i + 1][j - 1] + acc_cells[i + 1][
-                                                  j + 1]) / 9)
-                        print("Intermediate avg_sum: ", avg_sum)
-                        if (avg_sum >= 33):
-                            print("For radius: ", r, "average: ", avg_sum, "\n")
-                            circles.append((i, j, r))
-                            acc_cells[i:i + 5, j:j + 7] = 0
-    return circles
-"""
 
