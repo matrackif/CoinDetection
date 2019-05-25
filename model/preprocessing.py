e4bd44dc94e83ef9fa135a7e642e2fa5a0092c5a
Witaut Bajaryn <vitaut.bayaryn@gmail.com>
2019-05-25 20:55:31 +0200

    preprocessing: Show image and masks for debugging

--- model/preprocessing.py
+++ model/preprocessing.py
@@ -27,8 +27,12 @@ def hough_transform(img_file: str, greyscale: bool = False):
         output_img = cv2.imread(img_file, cv2.IMREAD_GRAYSCALE)
     else:
         output_img = cv2.imread(img_file, cv2.IMREAD_COLOR)
+    showdebug("Original image", output_img)
     mask = coin_color_mask(output_img)
+    showdebug("Merged mask", mask)
     only_coins = cv2.bitwise_and(output_img,output_img,mask=mask)
+    showdebug("Image after color detection", only_coins)
+    cv2.waitKey(0)
 
     grayscale_only_coins = cv2.cvtColor(only_coins, cv2.COLOR_BGR2GRAY)
     circles = cv2.HoughCircles(
@@ -59,24 +63,28 @@ def coin_color_mask(img):
         hsv,
         np.array([int(30/2), int(0.25*255), 0]),
         np.array([int(60/2), 255, 255]))
+    showdebug("coin yellow mask", coin_yellow_mask)
 
     # Detects the gray color in coins.
     coin_gray_mask = cv2.inRange(
         hsv,
         np.array([int(15/2), int(0.45*255), 0]),
         np.array([int(30/2), 255, 255]))
+    showdebug("coin gray mask", coin_gray_mask)
 
     # Detects the gray color in the ideal coin image.
     coin_ideal_gray_mask = cv2.inRange(
         hsv,
         np.array([int(45/2), 0, 0]),
         np.array([int(170/2), 255, 255]))
+    showdebug("coin ideal gray mask", coin_ideal_gray_mask)
 
     # Detects the almost-white gray color in the ideal coin image.
     coin_ideal_bright_gray_mask = cv2.inRange(
         hsv,
         np.array([int(20/2), int(0.03*255), 0]),
         np.array([int(65/2), int(0.05*255), 255]))
+    showdebug("coin ideal bright gray mask", coin_ideal_bright_gray_mask)
 
     mask = (
         coin_yellow_mask +
@@ -86,8 +94,12 @@ def coin_color_mask(img):
 
     # mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, np.ones((3,3),np.uint8))
     # mask = cv2.morphologyEx(mask, cv2.MORPH_DILATE, np.ones((3,3),np.uint8))
+    # showdebug("Opened and dilated mask", mask)
 
     return mask
+
+def showdebug(description: str, image):
+    cv2.imshow(description, cv2.resize(image, (800, 800)))
 
 
 def filter_found_cicles(circles):
